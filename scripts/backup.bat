e7acf8f25223047487020e71858fd2fdd8196e67
Firdaus Amin <furrydaus@gmail.com>
2017-03-25 06:54:50 +0800

    Server Backup `date +'m-H:S'`

---
+++ scripts/backup.bat
@@ -0,0 +1,9 @@
+echo Adding files into commit...
+	cd ../
+	git add * -A
+echo Adding Commit message...
+	git commit -m "Server Backup `date +'%Y-%m-%d %H:%M:%S'`"
+echo Pushing Commits to the 1.8.9 Branch...
+	git push -u origin 1.8.9
+echo BACKUP COMPLETED!
+exit
